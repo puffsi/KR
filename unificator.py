cdc9466a0aa97b8e810fbbc60ddf8cce8ae9965f
puffsi <53893273+puffsi@users.noreply.github.com>
2022-05-28 23:30:15 +0500

    Module for unification

--- unificator.py
+++ unificator.py
@@ -7,30 +7,30 @@ from PIL import Image
 import csv
 
 
-# создание csv и txt файла
-def unification():
-    training_dir = input('Укажите, откуда брать фото:')
-    gender = input('Укажите пол (F или M)')
-    path = input("Укажите полный путь назначения для помещения туда фотографий: ")
+
+def unification(path_to_unificate, gender, path_to_save):
+    configuration = open('config.txt', 'r')
+    text = configuration.readlines()
+    amount = ''
+    for i in text[26]:
+        if i != "\n":
+            amount = amount + i
+        else:
+            continue
+    amount = int(amount)
     numb = 1
     go_name = 1
-    f = open('filenames.txt', 'w')
-    with open('markup_file.csv', 'w', newline='') as csvfile:
-        while (numb <= 9641): #укажите количество изображений
+    while (numb <= amount): #укажите количество изображений
 
-            image_file_name = training_dir + '/' + str(numb) + '_' +  str(gender) + '_sketchers.jpeg'
-            for_txt = str(go_name) + "_" + str(gender) +  "_" + "sketchers" + ".jpeg"
-            filename = path + '/' + str(go_name) + "_" + str(gender) +  "_" + "sketchers" + ".jpeg"
-            im = Image.open(image_file_name).convert('L')
-            im = im.crop((10,150,225,340))
-            filewriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            filewriter.writerow([go_name, str(gender), 'sketchers'])
-            f.write(for_txt + '\n')
-            im.save(filename)
-            numb = numb + 1
-            go_name = go_name + 1
-    f.close()
-    print("Готово!")
+        image_file_name = path_to_unificate + '/' + str(numb) + '_' +  str(gender) + '_sketchers.jpeg'
+        if(gender == "F"):
+            filename = path_to_save + '/' + gender + '/' + str(go_name) + "_" + str(gender) +  "_" + "sketchers" + ".jpeg"
+        if(gender == "M"):
+            filename = path_to_save + '/' + gender + '/' + str(go_name) + "_" + str(gender) + "_" + "sketchers" + ".jpeg"
+        im = Image.open(image_file_name).convert('RGB')
+        im = im.crop((10,150,225,340))
+        im.save(filename)
+        numb = numb + 1
+        go_name = go_name + 1
+
     return 0
-
